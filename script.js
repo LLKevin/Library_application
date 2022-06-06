874db23f4f7a73fc6188f27b9a926b36eb717f3d
Kevin <kevinleo.lucas@gmail.com>
2022-06-05 23:51:52 -0400

    Add basic library functionality

--- script.js
+++ script.js
@@ -1,7 +1,74 @@
 let Library = [];
-function Book(){
-    // the constructor
+
+//get dom elements 
+let title = document.querySelector(".book-title");
+let author = document.querySelector(".book-author");
+let page_number = document.querySelector(".book-page-count");
+let isRead = document.querySelector(".isRead");
+let bookshelf = document.querySelector(".bookshelf");
+
+const Book = class{
+    constructor(title, author, pageNumber, isRead){
+        this.title = title;
+        this.author = author;
+        this.pageNumber = pageNumber;
+        this.isRead = isRead;
+    }
 }
-function addBookToLibrary(){
-    //do stuff;
+//flip the read status value
+Book.prototype.changeReadStatus = function(){
+    this.isRead = !this.isRead;
 }
+
+function addBookToLibrary(isRead){
+    Library.push(new Book(title.value, author.value, page_number.value, isRead))
+    displayLibrary();
+}
+
+function displayLibrary(){
+    for(let i = 0; i < Library.length; i++){
+
+        if ((bookshelf.rows.length - 1) > i) {
+            continue;
+          }
+
+        let row = bookshelf.insertRow(-1);
+        let titleCell = row.insertCell(-1);
+        let authorCell = row.insertCell(-1);
+        let pageNumberCell = row.insertCell(-1);
+        let isReadCell = row.insertCell(-1);
+        let deleteBook = row.insertCell(-1);
+        row.setAttribute('dataIndex', i);
+
+        // create a delete button 
+        let delete_book_button = document.createElement("button");
+        delete_book_button.innerHTML = "Delete Book"
+        delete_book_button.addEventListener("click",(event) =>{
+        
+        })
+        isReadCell.addEventListener("click",function(){
+            Book.changeReadStatus();
+        })
+
+        authorCell.innerHTML = Library[i].author;
+        titleCell.innerHTML = Library[i].title;
+        pageNumberCell.innerHTML = Library[i].pageNumber;
+        isReadCell.innerHTML = Library[i].isRead;
+       
+        //isReadCell.appendChild(btnChangeRead);
+        deleteBook.appendChild(delete_book_button);
+    }
+}
+
+function deleteBook(){
+    
+}
+
+let btnAddBook = document.querySelector("#addBook").addEventListener("click", (event) => {
+    event.preventDefault();
+  
+    let checkbox_IsRead = document.querySelector('input[name = "isRead"]:checked');
+    if (author.value !== "" && title.value !== "" && page_number.value !== "" && isRead.value !== undefined) {        
+        addBookToLibrary(checkbox_IsRead == null ? "has not been read" : "has been read" );
+    }
+  });
